daad5a5466c16cd37ca9f9b7704447acc49b5280
Karol <git@koehn.pro>
2023-02-20 20:55:16 +0100

    initial commit

---
+++ lib/dachsfisch.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'dachsfisch/version'
+
+module Dachsfisch
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dachsfisch/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Dachsfisch
+  VERSION = '0.1.0'
+end
