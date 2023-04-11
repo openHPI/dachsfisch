a6d509b3ac0490841255902f57408c8c2f97af2e
Karol <git@koehn.pro>
2023-04-11 13:39:03 +0200

    use oneliner syntax for specs

--- spec/dachsfisch/xml2_json_converter_spec.rb
+++ spec/dachsfisch/xml2_json_converter_spec.rb
@@ -6,95 +6,66 @@ RSpec.describe Dachsfisch::XML2JSONConverter do
 
     let(:json_hash) { JSON.parse(json) }
     let(:converter) { described_class.new xml: }
+    let(:xml) { example.xml }
 
     context 'with example 2' do
       let(:example) { Examples::Example2 }
-      let(:xml) { example.xml }
 
-      it 'converts to correct json' do
-        expect(json).to be_an_equal_json_as(example.json)
-      end
+      it { is_expected.to be_an_equal_json_as(example.json) }
     end
 
     context 'with example 3' do
       let(:example) { Examples::Example3 }
-      let(:xml) { example.xml }
 
-      it 'converts to correct json' do
-        expect(json).to be_an_equal_json_as(example.json)
-      end
+      it { is_expected.to be_an_equal_json_as(example.json) }
     end
 
     context 'with example 4' do
       let(:example) { Examples::Example4 }
-      let(:xml) { example.xml }
 
-      it 'converts to correct json' do
-        expect(json).to be_an_equal_json_as(example.json)
-      end
+      it { is_expected.to be_an_equal_json_as(example.json) }
     end
 
     context 'with example 5' do
       let(:example) { Examples::Example5 }
-      let(:xml) { example.xml }
 
-      it 'converts to correct json' do
-        expect(json).to be_an_equal_json_as(example.json)
-      end
+      it { is_expected.to be_an_equal_json_as(example.json) }
     end
 
     context 'with example 7' do
       let(:example) { Examples::Example7 }
-      let(:xml) { example.xml }
 
-      it 'converts to correct json' do
-        expect(json).to be_an_equal_json_as(example.json)
-      end
+      it { is_expected.to be_an_equal_json_as(example.json) }
     end
 
     context 'with example 8' do
       let(:example) { Examples::Example8 }
-      let(:xml) { example.xml }
 
-      it 'converts to correct json' do
-        expect(json).to be_an_equal_json_as(example.json)
-      end
+      it { is_expected.to be_an_equal_json_as(example.json) }
     end
 
     context 'with example 9' do
       let(:example) { Examples::Example9 }
-      let(:xml) { example.xml }
 
-      it 'converts to correct json' do
-        expect(json).to be_an_equal_json_as(example.json)
-      end
+      it { is_expected.to be_an_equal_json_as(example.json) }
     end
 
     context 'with multiple text nodes' do
       let(:example) { Examples::CustomExampleMultipleTextNodes }
-      let(:xml) { example.xml }
 
-      it 'converts to correct json' do
-        expect(json).to be_an_equal_json_as(example.json)
-      end
+      it { is_expected.to be_an_equal_json_as(example.json) }
     end
 
     context 'with a comment' do
       let(:example) { Examples::CustomExampleComment }
-      let(:xml) { example.xml }
 
-      it 'converts to correct json' do
-        expect(json).to be_an_equal_json_as(example.json)
-      end
+      it { is_expected.to be_an_equal_json_as(example.json) }
     end
 
     context 'with cdata' do
       let(:example) { Examples::CustomExampleCdata }
-      let(:xml) { example.xml }
 
-      it 'converts to correct json' do
-        expect(json).to be_an_equal_json_as(example.json)
-      end
+      it { is_expected.to be_an_equal_json_as(example.json) }
     end
   end
 end
