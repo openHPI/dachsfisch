b74c6e559f0d8f15513c93c6dc7f53bf83253ca2
Karol <git@koehn.pro>
2023-04-11 14:29:55 +0200

    use a loop to unclutter specs

--- spec/dachsfisch/xml2_json_converter_spec.rb
+++ spec/dachsfisch/xml2_json_converter_spec.rb
@@ -4,68 +4,14 @@ RSpec.describe Dachsfisch::XML2JSONConverter do
   describe '#perform' do
     subject(:json) { converter.perform }
 
-    let(:json_hash) { JSON.parse(json) }
     let(:converter) { described_class.new xml: }
-    let(:xml) { example.xml }
 
-    context 'with example 2' do
-      let(:example) { Examples::Example2 }
+    Examples.each do |example|
+      context "with #{example.name}" do
+        let(:xml) { example.xml }
 
-      it { is_expected.to be_an_equal_json_as(example.json) }
-    end
-
-    context 'with example 3' do
-      let(:example) { Examples::Example3 }
-
-      it { is_expected.to be_an_equal_json_as(example.json) }
-    end
-
-    context 'with example 4' do
-      let(:example) { Examples::Example4 }
-
-      it { is_expected.to be_an_equal_json_as(example.json) }
-    end
-
-    context 'with example 5' do
-      let(:example) { Examples::Example5 }
-
-      it { is_expected.to be_an_equal_json_as(example.json) }
-    end
-
-    context 'with example 7' do
-      let(:example) { Examples::Example7 }
-
-      it { is_expected.to be_an_equal_json_as(example.json) }
-    end
-
-    context 'with example 8' do
-      let(:example) { Examples::Example8 }
-
-      it { is_expected.to be_an_equal_json_as(example.json) }
-    end
-
-    context 'with example 9' do
-      let(:example) { Examples::Example9 }
-
-      it { is_expected.to be_an_equal_json_as(example.json) }
-    end
-
-    context 'with multiple text nodes' do
-      let(:example) { Examples::CustomExampleMultipleTextNodes }
-
-      it { is_expected.to be_an_equal_json_as(example.json) }
-    end
-
-    context 'with a comment' do
-      let(:example) { Examples::CustomExampleComment }
-
-      it { is_expected.to be_an_equal_json_as(example.json) }
-    end
-
-    context 'with cdata' do
-      let(:example) { Examples::CustomExampleCdata }
-
-      it { is_expected.to be_an_equal_json_as(example.json) }
+        it { is_expected.to be_an_equal_json_as(example.json) }
+      end
     end
   end
 end
