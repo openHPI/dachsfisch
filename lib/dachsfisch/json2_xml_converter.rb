639eda0eadbb8d30ab04415a4c0d9264893b22f6
Karol <git@koehn.pro>
2023-04-26 23:51:42 +0200

    - fix namespace examples - added gem to match xmls

--- lib/dachsfisch/json2_xml_converter.rb
+++ lib/dachsfisch/json2_xml_converter.rb
@@ -3,21 +3,25 @@
 module Dachsfisch
   class JSON2XMLConverter
     def initialize(json:)
-      # @json = json
       @json_hash = JSON.parse json
     end
 
     def add_element(xml, element)
       return unless element.is_a? Hash
 
+      @namespaces = {}
       element.each do |key, value|
-        add_node(xml, key, value)
+        add_node(xml, key, value) unless key == '@xmlns'
       end
     end
 
     def perform
       Nokogiri::XML::Builder.new do |xml|
         add_element xml, @json_hash
+
+        @namespaces.each do |namespace_key, namespace|
+          xml.doc.root[namespace_key] = namespace
+        end
       end.to_xml
     end
 
@@ -29,25 +33,26 @@ module Dachsfisch
         node = create_node(element, key, xml)
         element.keys.filter { |element_key| element_key.start_with?('@') }.each do |attribute_key|
           if attribute_key.start_with? '@xmlns'
+
             element[attribute_key].each do |namespace_key, namespace|
-              node["xmlns#{namespace_key == '$' ? '' : ":#{namespace_key}"}"] = namespace
+              @namespaces["xmlns#{namespace_key == '$' ? '' : ":#{namespace_key}"}"] = namespace
             end
           else
             node[attribute_key.delete_prefix('@')] = element[attribute_key]
           end
+
         end
       when Array
         element.each do |sub_element|
           add_node xml, key, sub_element
-          # xml.send(ns_key) { add_element(xml, sub_element) }
         end
-      else
-        raise "WTF #{element} is a #{element.class}" # deleteme
       end
     end
 
     def create_node(element, key, xml)
-      return xml.send(key, element['$']) if element.keys.include?('$')
+      if element.keys.include?('$')
+        return xml.send(key, element['$'])
+      end
 
       xml.send(key) { add_element(xml, element) }
     end
