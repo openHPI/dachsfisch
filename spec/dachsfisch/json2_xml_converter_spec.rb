beb5fc7850b2f2a01ec41f9dac32a03ff5f9ec15
Karol <git@koehn.pro>
2023-04-27 23:25:57 +0200

    - fix json to xml converter to accommodate custom changes and edge cases

--- spec/dachsfisch/json2_xml_converter_spec.rb
+++ spec/dachsfisch/json2_xml_converter_spec.rb
@@ -4,63 +4,13 @@ RSpec.describe Dachsfisch::JSON2XMLConverter do
   describe '#perform' do
     subject(:xml) { converter.perform }
 
-    let(:json) { example.json }
     let(:converter) { described_class.new json: }
 
-    context 'with example 2' do
-      let(:example) { Examples::Example2 }
+    Examples.each do |example|
+      context "with #{example.name}" do
+        let(:json) { example.json }
 
-      it 'converts to correct xml' do
-        expect(xml).to be_equivalent_to(example.xml)
-      end
-      # xml.alice 'bob'
-    end
-
-    context 'with example 3' do
-      let(:example) { Examples::Example3 }
-
-      it 'converts to correct xml' do
-        expect(xml).to be_equivalent_to(example.xml)
-      end
-    end
-
-    context 'with example 4' do
-      let(:example) { Examples::Example4 }
-
-      it 'converts to correct xml' do
-        expect(xml).to be_equivalent_to(example.xml)
-      end
-    end
-
-    context 'with example 5' do
-      let(:example) { Examples::Example5 }
-
-      it 'converts to correct xml' do
-        expect(xml).to be_equivalent_to(example.xml)
-      end
-    end
-
-    context 'with example 7' do
-      let(:example) { Examples::Example7 }
-
-      it 'converts to correct xml' do
-        expect(xml).to be_equivalent_to(example.xml)
-      end
-    end
-
-    context 'with example 8' do
-      let(:example) { Examples::Example8 }
-
-      it 'converts to correct xml' do
-        expect(xml).to be_equivalent_to(example.xml)
-      end
-    end
-
-    context 'with example 9' do
-      let(:example) { Examples::Example9 }
-
-      it 'converts to correct xml' do
-        expect(xml).to be_equivalent_to(example.xml)
+        it { is_expected.to be_equivalent_to(example.xml) }
       end
     end
   end
